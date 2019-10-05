55297aa28e265c3558f4f09523d5874eacd4fb76
Nathan Alex <nate745@gmail.com>
2019-10-05 09:33:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
         }
      }
 
-programmer_hash[:alan_kay][:known_for]= "alans_new_info = GUI"
+programmer_hash[:alan_kay][:known_for]= ["alans_new_info = GUI"]
 return programmer_hash
 
 end
