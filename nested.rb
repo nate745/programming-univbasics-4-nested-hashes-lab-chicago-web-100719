e67c5960db883115c3fca45b591aa728ae097756
Nathan Alex <nate745@gmail.com>
2019-10-05 09:24:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,11 +82,10 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] =>{
-		:known_for => "Ruby",
-		:languages => ["LISP", "C"]
-}
-
+		 		programmer_hash[:yukihiro_matsumoto] =>{
+						:known_for => "Ruby",
+						:languages => ["LISP", "C"]
+					}
 return programmer_hash
 end
 
